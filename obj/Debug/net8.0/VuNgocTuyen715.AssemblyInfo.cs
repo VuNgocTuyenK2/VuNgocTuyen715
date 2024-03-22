ec7c3dabee872e4e33823d26418832702f715c81
VuNgocTuyenK2 <tuyeni02km2@gmail.com>
2024-03-22 13:48:43 +0700

    update _layout

--- obj/Debug/net8.0/VuNgocTuyen715.AssemblyInfo.cs
+++ obj/Debug/net8.0/VuNgocTuyen715.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VuNgocTuyen715")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f3ebd3ea378749452c73452b72ab6fd72e2fd33")]
 [assembly: System.Reflection.AssemblyProductAttribute("VuNgocTuyen715")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VuNgocTuyen715")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
